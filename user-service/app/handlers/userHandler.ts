88157be1883be26bd9350a438cb800a9be990c23
--add <tarun@iotric.com>
2023-09-26 20:25:03 +0530

    user-service

--- user-service/app/handlers/userHandler.ts
+++ user-service/app/handlers/userHandler.ts
@@ -1,12 +1,69 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import middy from "@middy/core";
+import { UserService } from "../service/userService";
+import { ErrorResponse } from "../utility/response";
+import bodyParser from "@middy/http-json-body-parser";
+import { container } from "tsyringe";
+
+const service = container.resolve(UserService);
 
 export const Signup = middy((event: APIGatewayProxyEventV2) => {
-    console.log(event);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "response form signup"
-        })
+    return service.CreateUser(event);
+}).use(bodyParser());
+
+export const Login = middy((event: APIGatewayProxyEventV2) => {
+    return service.UserLogin(event);
+}).use(bodyParser());
+
+
+
+export const Verify = async (event: APIGatewayProxyEventV2) => {
+    const httpMethod = event.requestContext.http.method.toLowerCase();
+    if (httpMethod === "post") {
+        return service.VerifyUser(event);
+    } else if (httpMethod === "get") {
+        return service.GetVerificationToken(event);
+    } else {
+        return ErrorResponse(404, "requested method is not supported!");
     }
-})
+};
+
+export const Profile = async (event: APIGatewayProxyEventV2) => {
+    const httpMethod = event.requestContext.http.method.toLowerCase();
+    if (httpMethod === "post") {
+        return service.CreateProfile(event);
+    } else if (httpMethod === "put") {
+        return service.EditProfile(event);
+    } else if (httpMethod === "get") {
+        return service.GetProfile(event);
+    } else {
+        return ErrorResponse(404, "requested method is not supported!");
+    }
+};
+
+export const Cart = async (event: APIGatewayProxyEventV2) => {
+    const httpMethod = event.requestContext.http.method.toLowerCase();
+    if (httpMethod === "post") {
+        return service.CreateCart(event);
+    } else if (httpMethod === "put") {
+        return service.UpdateCart(event);
+    } else if (httpMethod === "get") {
+        return service.GetCart(event);
+    } else {
+        return ErrorResponse(404, "requested method is not supported!");
+    }
+};
+
+
+export const Payment = async (event: APIGatewayProxyEventV2) => {
+    const httpMethod = event.requestContext.http.method.toLowerCase();
+    if (httpMethod === "post") {
+        return service.CreatePaymentMethod(event);
+    } else if (httpMethod === "put") {
+        return service.UpdatePaymentMethod(event);
+    } else if (httpMethod === "get") {
+        return service.GetPaymentMethod(event);
+    } else {
+        return ErrorResponse(404, "requested method is not supported!");
+    }
+};
